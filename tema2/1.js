4593a9d505d212fbe1bc4021166d550777ed8d94
Alfredo <alfred.91.mc@gmail.com>
2023-10-11 14:00:25 +0200

    1

--- tema2/1.js
+++
@@ -1,4 +0,0 @@
-//Mostrar con un alert el primer mensaje.
-console.log("Hola mundo");
-//Mostrar con un alert el segundo mensaje.
-alert("Hola mundo esto es un Alert");
--- tema2/3.js
+++ tema2/3.js
@@ -6,14 +6,14 @@ primera=parseFloat(primera);
 
 segunda=parseFloat(segunda);
 
-var suma=primera+segunda;
+    var suma=primera+segunda;
 
-var producto=primera*segunda;
+    var producto=primera*segunda;
 
-var diferencia=primera-segunda;
+    var diferencia=primera-segunda;
 
-var division=primera/segunda;
+    var division=primera/segunda;
 
-var resto = primera%segunda;
+    var resto = primera%segunda;
 
 document.write("Primera: ",primera," Segunda: ",segunda, " Suma: ",suma," Producto: ",producto," Diferencia: ",diferencia," Division: ",division," Resto: ",resto);
