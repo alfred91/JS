4cb4017c992fcb76b9179d5bc0ccd27bb95ce26d
alfred91 <alfred.91.mc@gmail.com>
2023-12-11 17:45:24 +0100

    .

---
+++ tema5/GestionEventos/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function()){
+    const formulario = document.forms("formulario");
+    let intentos = parseint(getCookie("intentos") || 0;
+    mostrarIntentos();
+}
